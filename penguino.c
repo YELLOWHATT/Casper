c607bbf1c02c4d440536723dfa03af3a06ddf871
Haunting Spectre <43715379+YELLOWHATT@users.noreply.github.com>
2018-12-31 16:40:10 -0700

    New version

--- penguino.c
+++ penguino.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
-#include <string.h>
-
-/*
-
-* * * * * * * * * * * * * * * * * * * *
- * * * * * * * * * * * * * * * * * * *
-*                                     *
- *         Author: YELLLOWHATT       *
-*                                     * 
- *         Version: 0.0.3            *
-*                                     * 
- * * * * * * * * * * * * * * * * * * * 
-* * * * * * * * * * * * * * * * * * * * 
-
-*/
-
-char* c_name = "Casper"; 
+#include <string.h> 
+ 
+char *c_name = "Casper"; 
 char new_c_name[90]; 
 
 struct sys_var
 {
 
-    char menus[90];
+    char menus[70];
     char menu[90];
     char name[90]; 
 
@@ -30,8 +16,8 @@ struct sys_var
 struct currency_var
 {
 
-    char currencies[130]; 
-    char list_exit[5];
+    char currencies[70]; 
+    char list_exit[10];
     int currency_selection;
 
 };
@@ -45,10 +31,11 @@ int currency(void)
     const double pound_value = 0.79; 
     const double yen_value = 110.79; 
     const double swiss_value = 0.98; 
+    const double russian_value = 69.43; 
 
-    double y_p_v, y_e_v, y_y_v, y_s_v, y_d_v;  // Your currency value 
+    double y_p_v, y_e_v, y_y_v, y_s_v, y_r_v, y_d_v;  // Your currency value 
 
-    struct currency_var current = {"1 - Euro, 2 - Pound, 3 - Yen, or 4 - Swiss Franc"};
+    struct currency_var current = {"1 - Euro, 2 - Pound, 3 - Yen, 4 - Swiss Franc, or 5 - Russian Ruble"};
 
     printf("**********************************\n");
     printf("Welcome to the currency converter\n");
@@ -69,7 +56,7 @@ int currency(void)
 
             case 1: 
                 
-                printf("\n\nAll currencies will be converted into United States Dollars\n\n"); 
+                printf("\nAll currencies will be converted into United States Dollars\n\n"); 
                 printf("Please enter the amount of Euros you have: "); 
                 scanf("%lf", &y_e_v);
                 
@@ -79,7 +66,7 @@ int currency(void)
 
             case 2: 
 
-                printf("\n\nAll currencies will be converted into United States Dollars\n\n"); 
+                printf("\nAll currencies will be converted into United States Dollars\n\n"); 
                 printf("Please enter the amount of Pounds you have: "); 
                 scanf("%lf", &y_p_v); 
                 
@@ -89,7 +76,7 @@ int currency(void)
 
             case 3: 
 
-                printf("\n\nAll currencies will be converted into United States Dollars\n\n"); 
+                printf("\nAll currencies will be converted into United States Dollars\n\n"); 
                 printf("Please enter the amount of Yen you have: "); 
                 scanf("%lf", &y_y_v); 
                 
@@ -99,12 +86,22 @@ int currency(void)
 
             case 4: 
 
-                printf("\n\nAll currencies will be converted into United States Dollars\n\n");
+                printf("\nAll currencies will be converted into United States Dollars\n\n");
                 printf("Please enter the amount of Swiss Francs you have: "); 
                 scanf("%lf", &y_s_v); 
                 
                 y_d_v = y_s_v / swiss_value; 
                 printf("\n\nYou have %lf US Dollars\n", y_d_v);
+                break; 
+
+            case 5: 
+
+                printf("\nAll currencies will be converted into United States Dollars\n\n");
+                printf("Please enter the amount of Russian Roubles you have: "); 
+                scanf("%lf", &y_r_v); 
+
+                y_d_v = y_r_v / russian_value; 
+                printf("\n\nYou have %lf US Dollars\n", y_d_v); 
                 break; 
 
             default: 
@@ -193,16 +190,16 @@ int main(void)
 
     char change; 
 
-    struct sys_var var = {"Calculator, Currency converter or Exit"};  
+    struct sys_var var = {"Calculator, Currency converter, or Exit"};  
 
     printf("Hello\n\n"); 
-    printf("What is your name?\n\n"); 
+    printf("What is your name? (MAX NAME LIMIT : 90 char)\n\n"); 
     scanf("%[^\n]s", var.name); 
     
     printf("\nHello %s!!!\n", var.name); 
     printf("\nMy name is %s I am this program\n", c_name); 
     
-    printf("\nWould you like to change my name enter y or n: ");
+    printf("\nWould you like to change my name enter (y) or (n): ");
     scanf(" %c", &change);
 
     if (change == 'y')
@@ -211,18 +208,18 @@ int main(void)
         printf("\nPlease select a new name: ");
         scanf(" %[^\n]s", new_c_name); 
         c_name = new_c_name; 
-        printf("%s", new_c_name);
+        printf("\nNew name selected: %s", new_c_name);
 
     } 
 
     else 
     {
 
-        printf("\nSince you have not chosen y it will stay %s\n", c_name); 
+        printf("\nSince you have not chosen (y) it will stay %s", c_name); 
 
     }
     
-    printf("\nSelect one of the menus please (%s)?", var.menus); 
+    printf("\n\nSelect one of the menus please (%s)?", var.menus); 
     printf("\n\nEnter the name of the menu or enter anything to exit: ");  
     scanf(" %[^\n]s", var.menu); 
 
